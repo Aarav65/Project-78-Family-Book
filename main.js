c92d9bc9aca6bd11a21eab339abb674b307c3c63
Aarav65 <76677576+Aarav65@users.noreply.github.com>
2021-03-10 21:49:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var abouts=[
-    "Mr.Narendra Awasthi. he is Dealer of Indian Oil Corporation Ltd. and he is my Grandfather",
+    "Mr.Narendra Awasthi. He is MSc in Botony. At present he is Minister in Vaypar Mandal U.P. In the age of 74 he is still working as a farmer. ",
     "Mrs.Meera Aawasthi. She is a House Wife and she is my Grandmother",
     "Mr.Vineet Awasthi.He is C.A (Charted Accountant) and my father.",
     "Mrs.Rupal Awasthi. She is a House Wife and my mother",
